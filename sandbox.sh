97c217752514a3975e27a3df4c14b2b4e212194b
Jesse Stilwell <jesse.stilwell@gmail.com>
2020-06-12 23:49:12 -0400

    Add new method to drop sandbox database

--- sandbox.sh
+++ sandbox.sh
@@ -4,7 +4,7 @@ rsync -avv --progress --exclude=/config.php /mnt/code/www/moodle_prod/ /mnt/code
 echo "Refreshing data..."  
 sudo /rlscripts/moodle/moodle_sandbox_refresh_data --dirsrc=/mnt/data/moodledata_prod --dirdest=/mnt/data/moodledata_sand --excludes=sessions,cache,localcache,muc,lock -y
 echo "Preparing sandbox database..."
-sudo mysqladmin drop moodle_sand && sudo mysqladmin create moodle_sand
+sudo mysql -e "DROP DATABASE moodle_sand" && sudo mysqladmin create moodle_sand
 echo "Refreshing database without logstore table..."
 sudo mysqldump -v moodle_prod --no-data | sudo mysql moodle_sand && sudo mysqldump -v moodle_prod --no-create-info --ignore-table=moodle_prod.mdl_logstore_standard_log | sudo mysql moodle_sand
 echo "Running upgrade script..."
