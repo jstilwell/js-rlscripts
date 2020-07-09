422e34d60d2313a085d728e573a530465c87b320
Jesse Stilwell <jesse.stilwell@gmail.com>
2020-07-09 01:24:53 -0400

    Add bounce to known hosts

--- unregister.sh
+++ unregister.sh
@@ -4,6 +4,7 @@ username=$(printenv LOGNAME)
 echo "Unregisterring VM (${site_id})"
 yes Y | /rlscripts/system/system_unregister
 echo "Removing UTR Monitoring for VM (${site_id})"
-yes | ssh ${username}@aws-bounce.remote-learner.net "rlcloud monitoring del ${site_id}"
+ssh-keyscan aws-bounce.remote-learner.net >> ~/.ssh/known_hosts
+ssh ${username}@aws-bounce.remote-learner.net "rlcloud monitoring del ${site_id}"
 echo "Shutting down VM -> rl_decom_site -s ${site_id}"
 sudo shutdown -h now
