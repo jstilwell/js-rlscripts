1bfc4b496dd4181dbc0dab29e7134ae66e5ce14b
Jesse Stilwell <jesse.stilwell@gmail.com>
2020-07-09 01:20:45 -0400

    Add shutdown to script

--- unregister.sh
+++ unregister.sh
@@ -2,6 +2,8 @@
 site_id=$(sed -n 's/site_id = //p' /mnt/data/conf/global.ini)
 username=$(printenv LOGNAME)
 echo "Unregisterring VM (${site_id})"
-/rlscripts/system/system_unregister
+yes Y | /rlscripts/system/system_unregister
 echo "Removing UTR Monitoring for VM (${site_id})"
-ssh ${username}@aws-bounce.remote-learner.net "rlcloud monitoring del ${site_id}"
+yes | ssh ${username}@aws-bounce.remote-learner.net "rlcloud monitoring del ${site_id}"
+echo "Shutting down VM -> rl_decom_site -s ${site_id}"
+sudo shutdown -h now
