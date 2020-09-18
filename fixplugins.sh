0adba7af76f93d04caa01260cece9a7a2fe4337d
Jesse Stilwell <jesse.stilwell@gmail.com>
2020-09-18 19:46:02 -0400

    Create fixplugins.sh

---
+++ fixplugins.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# RL Plugin Fixer
+# Author: Jesse Stilwell <jesse.stilwell@remote-learner.com>
+
+# USAGE:
+# ./fixplugins.sh /mnt/code/www/moodle_prod MOODLE_39_STABLE
+
+codedir=$1
+gitbranch=$2
+filename="dirlist.txt"
+
+# Prompt for git password one time only.
+ssh-add /root/.ssh/id_rsa.rlgit
+
+# Get list of subdirectories with .git folders (external plugins)
+find $codedir -name ".git" -type d > $filename
+
+while read line; do
+# Change directory and move one down. 
+cd $line && cd ..
+# Checkout branch specified by user.
+git fetch origin && git checkout -b $gitbranch origin/$gitbranch
+done < $filename
